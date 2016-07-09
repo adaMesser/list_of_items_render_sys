e1aa736208be648b2efddbacc318775d0f53adde
ada_messer <ladydowaakhin@gmail.com>
2016-07-10 01:50:42 +0300

    new homework #21

---
+++ list_of_items.api.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * hook list of items menu
+ * 
+ * @param $route
+ * @param $item
+ */
+function hook_list_of_items_menu($route, $item) {
+
+}
+
+/**
+ * @param $subitems
+ * @param $route
+ * @param $item
+ */
+function hook_list_of_items_menu_alter(&$subitems, $route, $item) {
+
+}
