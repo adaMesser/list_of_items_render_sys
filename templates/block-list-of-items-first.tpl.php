9a77f596c55bbfa7de64292cd2888fc2a44514c9
ada_messer <ladydowaakhin@gmail.com>
2016-07-14 16:23:49 +0300

    list of items with render array (2 tpl)

--- templates/block-list-of-items-first.tpl.php
+++ templates/block-list-of-items-first.tpl.php
@@ -1,15 +1,5 @@
 <?php
   if (!empty($list_block['message1'])) {
-    hide($list_block['message1']);
-  }
-?>
-<div class="test-messages-wrapper">
-  <?php print drupal_render_children($list_block); ?>
-</div>
-
-<?php
-  if (!empty($list_block['message1'])) {
-    show($list_block['message1']);
     print drupal_render($list_block['message1']);
   }
 ?>
